b63b59e5993b54bc1400500a2ea0f67261d5120c
Alex <alex.kucherenkowb@gmaill.com>
2019-08-12 11:47:54 +0300

    Angular registration

--- DataModels/User.cs
+++ DataModels/User.cs
@@ -3,10 +3,8 @@ namespace DataModels
     public class User
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Username { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
